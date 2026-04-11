c3b3edcc0f67649a6993acb47b2b7d47602efd65
Nour123amer <nouramer295@gmail.com>
2026-04-11 07:47:54 -0400

    edit production and development products in upgrade component

--- src/components/sidebar/upgrade.tsx
+++ src/components/sidebar/upgrade.tsx
@@ -7,23 +7,36 @@ export default function Upgrade() {
 
   const upgrade = async () => {
     const session = await authClient.getSession();
-    console.log("session =>", session)
+    console.log("session =>", session);
+
+    let products = process.env.NODE_ENV === "development" ? [
+      "5d5772f0-fe96-42ff-8d9d-2bff6f1e7ffa", 
+          "9f616e5c-8a36-448c-801a-c82a6ee07426",
+          "bf914353-33dc-41fe-9b6b-6f10c7a6687a",
+    ]:[
+      "e70fef4c-942a-4098-8d26-f31b3307f0ed",
+      "dca88f28-596a-4e22-89b6-1b0f6d0f9fc5",
+      "a8ff4fa7-4d11-414d-a498-69eeaa5b1b74"
+    ];
 
     if (!session?.data?.user) {
       console.log("User not logged in");
       return;
     }
 
+
+
     try {
       await authClient.checkout({
-        products: [
-          "6c84189e-1ce2-4214-ac43-b439bb1e544e", 
-          "038b2068-adb4-4d8b-803c-640462f71ed4",
-          "dd2f6988-aa65-476f-abec-f25c2ba0c58b",
+        products: products,
+        //  [
+        //   "6c84189e-1ce2-4214-ac43-b439bb1e544e", 
+        //   "038b2068-adb4-4d8b-803c-640462f71ed4",
+        //   "dd2f6988-aa65-476f-abec-f25c2ba0c58b",
         //  "5d5772f0-fe96-42ff-8d9d-2bff6f1e7ffa", // 100 credits
         //  "9f616e5c-8a36-448c-801a-c82a6ee07426", // 200 credits  local
         //  "bf914353-33dc-41fe-9b6b-6f10c7a6687a", // 50 credits
-        ],
+        // ],
       });
     } catch (err) {
       console.error("Checkout error:", err);
